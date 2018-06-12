132193991bcd868f87cd3982d827f72f2d170080
gitbett <ericsbet@gmail.com>
2018-06-12 20:15:45 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +11,12 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+			  onFileSelect(event){
+			  console.log(event);
+
+			  }
+login(){
+	this.navCtrl.push(HomePage);
+}
 
 }
---
+++ src/pages/obituary/obituary.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the ObituaryPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-obituary',
+  templateUrl: 'obituary.html',
+})
+export class ObituaryPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ObituaryPage');
+  }
+
+			  onFileSelect(event){
+			  console.log(event);
+
+			  }
+			  login(){
+			this.navCtrl.push(HomePage);
+			}		
+
+}
